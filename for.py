f0340dba9147385689846183707e6850d77cdefe
danilovalerio <danilo_vs@hotmail.com>
2019-05-27 10:51:25 -0300

    for - iterar com comando de repetição

---
+++ for.py
@@ -0,0 +1,15 @@
+lista1 = [1,2,3,4,5,6]
+lista2 = ["Olá","mundo","!"]
+lista3 = [0,"ola","biscoito","bolacha","pudim",True]
+
+for i in lista1:
+    print(i)
+
+for i in lista2:
+    print(i)
+
+for i in lista3:
+    print(i)
+
+
+
