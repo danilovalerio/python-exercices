5e07f9044146047be6cf80d9f1ff3fcdc19d79c9
danilovalerio <danilo_vs@hotmail.com>
2019-05-27 12:07:55 -0300

    Funções - Escope e declaração

---
+++ funcoes.py
@@ -0,0 +1,13 @@
+def soma(x,y): #def palavra reservada para criar função em python
+    return x+y
+
+def mult(x,y):
+    return x*y
+
+print(soma(3,5))
+print(mult(3,5))
+
+s = soma(3,5)
+m = mult(3,5)
+
+print(soma(s,m))
