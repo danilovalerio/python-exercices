dcec54b413de37e1715a62a411d28f5f716b6125
danilovalerio <danilo_vs@hotmail.com>
2019-05-27 10:05:49 -0300

    Operadores relacionais

---
+++ operadoresRelacionais.py
@@ -0,0 +1,30 @@
+x = 10
+y = 5
+z = 10
+#Exemplos dos operadores relacionais
+# == igual 
+print(x == y)
+print(x == z)
+
+# != diferente
+print(x != y)
+print(x != z)
+
+# > maior 
+print(x > y)
+print(x > z)
+
+# < menor 
+print(x < y)
+print(x < z)
+
+# >= maior ou igual 
+print(x >= y)
+print(x >= z)
+
+# <= menor ou igual
+print(x <= y)
+print(x <= z)
+
+
+
