9bc5ea1947f77d5402dd488fc29035fd41cc54d4
danilovalerio <danilo_vs@hotmail.com>
2019-05-27 10:41:31 -0300

    Estrutura condicional com ELSE

--- condicional.py
+++ condicional.py
@@ -4,9 +4,13 @@ z = 10
 
 if x > y:
     print("X é maior que Y")
+else:
+    print("X é NÃO maior que Y")
 
 if y > z:
-    print("Y é maior que Z") #não vai exibir
+    print("Y é maior que Z") 
+else:
+    print("Y é MENOR que Z") 
 
 if z == x:
     print("Z é igual a X")
