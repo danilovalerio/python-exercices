b0e59ffb7fcc8e5e81467d4934f43ddb784aaf95
danilovalerio <danilo_vs@hotmail.com>
2019-05-27 14:26:17 -0300

    Dicionário - como criar

---
+++ dicionario.py
@@ -0,0 +1,9 @@
+#dicionário funciona como um "objeto JavaScript" chave: propriedade
+dicionario_sites = {"Danilo":"daninfodicas.wordpress.com",
+                    "Google":"www.google.com",
+                    "Udemy":"www.udemy.com"}
+
+for site in dicionario_sites:
+    print(site)
+
+print(dicionario_sites["Google"])
