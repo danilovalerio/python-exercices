8e04692b300dd11b30920feb31c1fa7c01440f83
danilovalerio <danilo_vs@hotmail.com>
2019-05-27 10:29:24 -0300

    Operadores lógicos

---
+++ operadoresLogicos.py
@@ -0,0 +1,21 @@
+x = 10
+y = 5
+z = 10
+
+#Exemplos dos operadores lógicos
+# AND
+print(x == y and x == z)
+print(x > y and z > y)
+
+# OR
+print(x == y or x == z)
+print(x > y or z > y)
+
+# # NOT
+print(not x == y)
+print(not x > y)
+
+
+
+
+
