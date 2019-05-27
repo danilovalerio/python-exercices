e3e8a038d24ba05fcf01f725d8ec6742eceec765
danilovalerio <danilo_vs@hotmail.com>
2019-05-27 10:56:20 -0300

    for com Range - função range

---
+++ for_range.py
@@ -0,0 +1,8 @@
+for i in range(10): #imprime 10 posições
+    print(i)
+
+for i in range(10,20): #imprime de 10 até 20 de 1 em 1
+    print(i)
+
+for i in range(10,20,2): #imprime de 10 até 20 indo de 2 em 2
+    print(i)
